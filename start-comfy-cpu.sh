bac93634eff73b880e46b28c02f85f955640f20c
Christian Zufferey <christian@zufferey.com>
2025-07-19 18:31:04 +0200

    new: ajouté les deux scripts pour démarrer l'interface Comfy

---
+++ start-comfy-cpu.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#Petit script pour démarrer facilement stable-diffusion-webui-docker dans une vm Ubuntu de  Proxmox, avec surtout la redirection du port sur le local network
+#zf250719.1816
+
+# source: 
+
+echo -e "Démarrage de stable-diffusion-webui-docker...
+
+"
+
+./stop.sh
+
+
+#cp ./share-start.sh ../fastsdcpu/
+
+cd ../stable-diffusion-webui-docker/
+socat TCP-LISTEN:8080,fork,reuseaddr TCP:127.0.0.1:7860 &
+#./share-start.sh
+#./start-webui.sh
+docker compose --profile comfy-cpu up --build &
+
+# &
+
+# > /tmp/fastsdcpu.log 2>&1 &
+
+
+
+exit
+
+echo -e "
+
+Fastsdcpu démarré !
+
+"
+
+#tail -f /tmp/fastsdcpu.log
+
--- start-comfy-gpu.sh
+++ start-comfy-gpu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Petit script pour démarrer facilement stable-diffusion-webui-docker dans une vm Ubuntu de  Proxmox, avec surtout la redirection du port sur le local network
-#zf250717.1742
+#zf250718.1437
 
 # source: 
 
@@ -16,11 +16,14 @@ echo -e "Démarrage de stable-diffusion-webui-docker...
 cd ../stable-diffusion-webui-docker/
 socat TCP-LISTEN:8080,fork,reuseaddr TCP:127.0.0.1:7860 &
 #./share-start.sh
-./start-webui.sh
+#./start-webui.sh
+docker compose --profile comfy up --build &
 
 # &
 
 # > /tmp/fastsdcpu.log 2>&1 &
+
+
 
 exit
 
